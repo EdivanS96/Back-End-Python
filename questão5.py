a456bfd09c2c4c7db140e4320999dd90ceea62d9
Edivan Silva <164213518+EdivanS96@users.noreply.github.com>
2025-08-29 14:39:10 -0300

    questão5.py

--- questão5.py
+++ questão5.py
@@ -1,9 +1,10 @@
 # Q5 - Gerar acrônimo
 frase = input("Digite um nome de curso ou expressão: ").lower()
 
-stopwords = {"de", "da", "do", "das", "dos", "e"}
+acro = {"de", "da", "do", "das", "dos", "e"}
 palavras = frase.split()
 
-acronimo = "".join([p[0].upper() for p in palavras if p not in stopwords])
+acronimo = "".join([p[0].upper() for p in palavras if p not in acro])
+
 
 print("Acrônimo:", acronimo)
